13dc2f53232e7bd014a49cd0974a83c6d674f73c
LeeJuyoung2015 <128190353+LeeJuyoung2015@users.noreply.github.com>
2023-06-10 22:34:18 +0900

    Update

--- JAVA_TEAM_12/src/Client/Person.java
+++ JAVA_TEAM_12/src/Client/Person.java
@@ -4,10 +4,6 @@ public class Person {
     public String name;
     public String photo;
 
-    public Person() {
-        // TODO Auto-generated constructor stub
-    }
-
     public Person(String name, String photo) {
         super();
         this.name = name;
